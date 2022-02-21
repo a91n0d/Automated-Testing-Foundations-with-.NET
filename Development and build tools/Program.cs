77ab58d8039ede087cda3d6994e35cabc08b36a9
Alexei Novikov <a91n0d@gmail.com>
2022-02-21 20:14:17 +0300

    Add project files.

---
+++ Development and build tools/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Development_and_build_tools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
