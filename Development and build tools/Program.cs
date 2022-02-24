fefb877519887f92535bc872429f4a66220acef7
Alexei Novikov <a91n0d@gmail.com>
2022-02-24 20:42:05 +0300

    Finish.

--- Development and build tools/Program.cs
+++ Development and build tools/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Text;
 
 namespace Development_and_build_tools
 {
     class Program
     {
+        //Написать программу, которая принимает из командной строки последовательность символов(строку)
+        //в качестве аргумента и выводит в консоль максимальное количество неодинаковых последовательных символов в строке.
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите последовательность символов: ");
+            string str = Console.ReadLine();
+            while (str.Length == 0)
+            {
+                Console.WriteLine("Вы ввели пустую строку, попробуйте еще раз: ");
+                str = Console.ReadLine();
+            }            
+            int len = str.Length;
+            var sb = new StringBuilder();
+            string maxUnequalChar = string.Empty;
+            for (int i = 0; i < len; i++)
+            {
+                if (sb.Length == 0 || str[i] != sb[sb.Length - 1])
+                {
+                    sb.Append(str[i]);
+                }
+                else
+                {
+                    if (maxUnequalChar.Length < sb.Length)
+                    {
+                        maxUnequalChar = sb.ToString();                        
+                    }
+                    sb.Clear();
+                    sb.Append(str[i]);
+                }
+
+                if (i == len - 1 && maxUnequalChar.Length < sb.Length)
+                {
+                    maxUnequalChar = sb.ToString();
+                }
+            }
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine($"Максимальное количество неодинаковых последовательных символов в строке: {maxUnequalChar}");
         }
     }
 }
